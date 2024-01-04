63ab32aef371a2f48bd7dd33aed489d5a125b6a1
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-01-04 01:23:30 -0500

    first commit

---
+++ src/Recursion.java
@@ -0,0 +1,115 @@
+/**
+ * @author Marissa Crevecoeur; UNI: mac2538
+ * Programming Assignment 2 - Recursion exercises
+ * COMS W3134
+ *
+ * Note: All methods must be written recursively. No credit will be given for
+ * methods written without recursion, even if they produce the correct output.
+ */
+
+public class Recursion {
+    /**
+     * Returns the value of x * y, computed via recursive addition.
+     * x is added y times. Both x and y are non-negative.
+     * @param x  non-negative integer multiplicand 1
+     * @param y  non-negative integer multiplicand 2
+     * @return   x * y
+     */
+    public static int recursiveMultiplication(int x, int y) {
+        // TODO
+        if(y ==0) return 0;
+        if(y ==1) return x;
+        return x + recursiveMultiplication(x, y-1);
+    }
+    /******************************************************************************/
+
+
+    /**
+     * Reverses a string via recursion.
+     * @param s  the non-null string to reverse
+     * @return   a new string with the characters in reverse order
+     */
+    public static String reverse(String s) {
+        // TODO
+        if(s.length() <= 1) return s;
+        return s.substring(s.length()-1) + reverse(s.substring(0,s.length()-1));
+    }
+    /******************************************************************************/
+
+
+    private static int maxHelper(int[] array, int index, int max) {
+        // TODO
+        if(index >= array.length) return max;
+        if(array[index] > max) max = array[index];
+        return maxHelper(array, index + 1, max);
+    }
+
+    /**
+     * Returns the maximum value in the array.
+     * Uses a helper method to do the recursion.
+     * @param array  the array of integers to traverse
+     * @return       the maximum value in the array
+     */
+    public static int max(int[] array) {
+        return maxHelper(array, 0, Integer.MIN_VALUE);
+    }
+    /******************************************************************************/
+
+
+    /**
+     * Returns whether or not a string is a palindrome, a string that is
+     * the same both forward and backward.
+     * @param s  the string to process
+     * @return   a boolean indicating if the string is a palindrome
+     */
+    public static boolean isPalindrome(String s) {
+        // TODO
+        if(s.length() <= 1) return true;
+        if(!s.substring(0,1).equals(s.substring(s.length()-1))){
+            return false;
+        }
+        return isPalindrome(s.substring(1,s.length()-1));
+    }
+    /******************************************************************************/
+
+
+    private static boolean memberHelper(int key, int[] array, int index) {
+        // TODO
+        if(array.length==0 || index >= array.length) return false;
+        if(array.length== 1 && array[index] != key) return false;
+        if(array[index] == key) return true;
+        return memberHelper(key, array, index+1);
+    }
+
+    /**
+     * Returns whether or not the integer key is in the array of integers.
+     * Uses a helper method to do the recursion.
+     * @param key    the value to seek
+     * @param array  the array to traverse
+     * @return       a boolean indicating if the key is found in the array
+     */
+    public static boolean isMember(int key, int[] array) {
+        return memberHelper(key, array, 0);
+    }
+/******************************************************************************/
+
+
+    /**
+     * Returns a new string where identical chars that are adjacent
+     * in the original string are separated from each other by a tilde '~'.
+     * @param s  the string to process
+     * @return   a new string where identical adjacent characters are separated
+     *           by a tilde
+     */
+    public static String separateIdentical(String s) {
+        // TODO
+        int i = 1;
+        if(s.length() < 2) return s;
+        if(s.substring(0,1).equals(s.substring(1,2))){
+            s = s.substring(0,1) + "~" + s.substring(1);
+            i = 2;
+        }
+        return s.substring(0,i) + separateIdentical(s.substring(i));
+    }
+
+}
